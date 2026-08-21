511ec4bb33e879de0b8e0643ec4fba1ab505322c
joeywong800 <joeywong800@gmail.com>
2026-08-21 15:54:22 +0200

    Update useSourceSelection.ts

--- src/components/player/hooks/useSourceSelection.ts
+++ src/components/player/hooks/useSourceSelection.ts
@@ -2,7 +2,7 @@ import {
   EmbedOutput,
   NotFoundError,
   SourcererOutput,
-} from "@p-stream/providers";
+} from "@joeywong800/providers";
 import { useAsyncFn } from "react-use";
 
 import { isExtensionActiveCached } from "@/backend/extension/messaging";
