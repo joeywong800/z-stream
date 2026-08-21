47f3e5dcc5ccd28e8c352806c61c3ced2b213960
joeywong800 <joeywong800@gmail.com>
2026-08-21 16:15:22 +0200

    Update vdrk.ts

--- src/utils/externalSubtitles/vdrk.ts
+++ src/utils/externalSubtitles/vdrk.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { labelToLanguageCode } from "@p-stream/providers";
+import { labelToLanguageCode } from "@z-stream/providers";
 
 import { CaptionListItem } from "@/stores/player/slices/source";
 
