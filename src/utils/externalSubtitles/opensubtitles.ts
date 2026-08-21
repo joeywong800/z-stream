7d40db621e5bcf7c14349b2216a5519bd69860e3
joeywong800 <joeywong800@gmail.com>
2026-08-21 16:03:38 +0200

    Update opensubtitles.ts

--- src/utils/externalSubtitles/opensubtitles.ts
+++ src/utils/externalSubtitles/opensubtitles.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { labelToLanguageCode } from "@p-stream/providers";
+import { labelToLanguageCode } from "@joeywong800/providers";
 
 import { CaptionListItem } from "@/stores/player/slices/source";
 
